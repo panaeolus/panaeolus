4a606c746c51722f653a3d2a500f92a3e2232dcd
hlolli <hlolli@gmail.com>
2019-05-26 15:54:21 +0200

    dump native to root dir during jar compilation

--- editor/src/app/main/core.cljs
+++ editor/src/app/main/core.cljs
@@ -7,7 +7,6 @@
             ["uri-js"]
             ))
 
-(println "1111")
 
 (def nrepl-port (+ 1025 (rand-int (- 65535 1025))))
 
@@ -18,22 +17,16 @@
 (.setApplicationMenu Menu nil)
 
 (defn init-browser []
-  (println "222222")
-  (prn "323232323")
   (reset! main-window (BrowserWindow.
                        (clj->js {:width 800
                                  :height 600
                                  :webPreferences {:nodeIntegration true}
                                  })))
   ;; (.toggleDevTools @main-window)
-  (println "333333")
-  ;; (.loadURL @main-window (str "file://" js/__dirname "/public/index.html"))
-  ;; (.loadFile @main-window (str "file://" js/__dirname "/public/index.html"))
-  (println "4444")
+  (.loadURL @main-window (str "file://" js/__dirname "/public/index.html"))
   (.on @main-window "closed" #(reset! main-window nil)))
 
 (defn boot-jre! [resolve reject]
-  (println "55555")
   (when-not @jre-connection
     (let [jre-conn (jre/spawn #js ["java"] "-jar" #js [(path/join js/__dirname "panaeolus-0.4.0-SNAPSHOT.jar") "nrepl" (str nrepl-port)])]
       (exit-hook (fn [] (.pause (.-stdin jre-conn)) (.kill jre-conn)))
@@ -43,23 +36,17 @@
                           (js/setTimeout #(resolve #js ["started" nrepl-port]) 100)
                           (print data)))))
       (.on (.-stderr jre-conn) "data" (fn [data] (println "error: " (.toString data))))
-      (println "66666")
       (reset! jre-connection jre-conn)
-      (println "77777")
       (.on jre-conn "close" #(do (reset! jre-connection nil) (println "JRE closed!"))))))
 
 (defn main []
-  (println "888888")
-  #_(.on ipcMain "boot-jre"
-         (fn [event arg]
-           (-> (new js/Promise (fn [resolve reject] (boot-jre! resolve reject)))
-               (.then (fn [data] (.reply event "nrepl" data))))))
-  (println "9999999")
+  (.on ipcMain "boot-jre"
+       (fn [event arg]
+         (-> (new js/Promise (fn [resolve reject] (boot-jre! resolve reject)))
+             (.then (fn [data] (.reply event "nrepl" data))))))
   (.on ipcMain "dev-reload" (fn [event arg] (prn "dev Reload") (.reply event "nrepl-port-num" (.reply event "nrepl" #js ["started" nrepl-port]))))
   (.on ipcMain "quit" (fn [_ _] (prn "quit1") (.quit app)))
   (.on app "ready" init-browser)
-  (println "10101010101")
-  (println "11 11 11 11")
   (.on app "window-all-closed" #(when-not (= js/process.platform "darwin") (prn "quit2") (.quit app))))
 
 
--- scripts/compile_jar.clj
+++ scripts/compile_jar.clj
@@ -4,6 +4,9 @@
             [badigeon.clean :as clean]
             [badigeon.compile :as compile]
             [badigeon.jar :as jar]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.walk :as walk]
             [clojure.tools.deps.alpha :as deps]
             [clojure.tools.deps.alpha.reader :as deps-reader]
             [panaeolus.all :as panaeolus]
@@ -71,6 +74,41 @@
 
 (def jline-jars
   (filter #(re-find #"jline" %) jars))
+
+(defn map-keys
+  "Apply f to each key in m"
+  [m f]
+  (reduce
+   (fn [acc [k v]] (assoc acc (f k) v))
+   {} m))
+
+(defn- canonicalize-sym [s]
+  (if (and (symbol? s) (nil? (namespace s)))
+    (as-> (name s) n (symbol n n))
+    s))
+
+(defn- canonicalize-all-syms
+  [deps-map]
+  (walk/postwalk
+   #(cond-> % (map? %) (map-keys canonicalize-sym))
+   deps-map))
+
+(defn- slurp-deps-edn []
+  (if (.exists (io/file "deps.edn"))
+    (deps-reader/slurp-deps "deps.edn")
+    (-> "deps.edn"
+        io/resource
+        slurp
+        edn/read-string
+        canonicalize-all-syms)))
+
+;; extract the native dependencies with badigeon
+(bundle/extract-native-dependencies
+ (System/getProperty "user.dir")
+ {:deps-map (slurp-deps-edn)
+  :allow-unstable-deps? true
+  :native-path "native"
+  :native-prefixes {'overtone/ableton-link ""}})
 
 (defn -main []
   (println "cleaning target...")
