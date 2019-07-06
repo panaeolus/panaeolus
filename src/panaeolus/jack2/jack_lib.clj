95cbc431037ed8fd512e37fbaf0e08361228b084
Hlodver Sigurdsson <hlolli@gmail.com>
2019-07-06 03:03:40 -0700

    jack windows, read-line on audio-interface

--- src/panaeolus/jack2/jack_lib.clj
+++ src/panaeolus/jack2/jack_lib.clj
@@ -2,6 +2,7 @@
   (:require
    [clojure.java.io :as io]
    [clojure.core.async :as async]
+   [panaeolus.config :refer [config]]
    [panaeolus.utils.jna-path :as jna-path])
   (:import
    [org.jaudiolibs.jnajack.lowlevel JackLibraryDirect]
@@ -20,31 +21,77 @@
 
 (def jack-server-atom (atom nil))
 
-(defn spawn-jackd-windows! []
+(defn kill-jackd-windows! []
+  (-> (ProcessBuilder. ^"[Ljava.lang.String;" (into-array String ["cmd.exe" "/c" "taskkill" "/F" "/IM" "jackd.exe"]))
+       (.start)
+	(.waitFor)))
+
+(defn spawn-jackd-windows! [interface-name] (prn "interface-name" interface-name)
   (let [kill-callback (fn [] (println "jackd.exe died"))]
     (async/thread
       (let [jackd-file (io/file jna-path/libcsound-cache-path "jackd.exe")
             jackd-loc (.getAbsolutePath jackd-file)
-            pbuilder (ProcessBuilder. ^"[Ljava.lang.String;" (into-array String ["cmd.exe" "/c" (str "\"" jackd-loc "\"") "-d" "portaudio" "-p" "2048" "-r" "44100"]))
+			_ (prn "DEBUG" (into ["cmd.exe" "/c" (str "\"" jackd-loc "\"") "-R" 
+					                             "-d" "portaudio" 
+												 "-p" (get-in @config [:jack :period])
+												 "-r" (:sample-rate @config)]
+					                            (if interface-name ["-d" interface-name] [])))
+            pbuilder (ProcessBuilder. ^"[Ljava.lang.String;" 
+			           (into-array String (into ["cmd.exe" "/c" (str "\"" jackd-loc "\"") "-R" 
+					                             "-d" "portaudio" 
+												 "-p" (get-in @config [:jack :period])
+												 "-r" (:sample-rate @config)]
+					                            (if interface-name ["-d" interface-name] []))))
             process  (.start pbuilder)]
         (reset! jack-server-atom process)
         (try (with-open [error-stream (clojure.java.io/reader (.getErrorStream process))]
                (loop []
                  (when-let [line (.readLine ^java.io.BufferedReader error-stream)]
-                   (println line)
+                   (println "ERROR" line)
                    (recur))))
              (catch java.io.IOException e kill-callback nil))
         (try (with-open [reader (clojure.java.io/reader (.getInputStream process))]
                (loop []
                  (when-let [line (.readLine ^java.io.BufferedReader reader)]
-                   (println line)
+                   (println "STDOUT" line)
                    (recur))))
              (catch java.io.IOException e kill-callback nil))))))
 
-(when (= :windows (jna-path/get-os))
-  (spawn-jackd-windows!)
-  (async/<!! (async/timeout 2000)))
+(defn get-jackd-interfaces-windows []
+  (kill-jackd-windows!)
+  (let [interfaces (atom [])
+        jackd-file (io/file jna-path/libcsound-cache-path "jackd.exe")
+        jackd-loc (.getAbsolutePath jackd-file)
+        process (-> (ProcessBuilder. ^"[Ljava.lang.String;" 
+                        (into-array String ["cmd.exe" "/c" (str "\"" jackd-loc "\"") "-d" "portaudio" "-l"]))
+                  (.start))]
+	(with-open [reader (clojure.java.io/reader (.getInputStream process))]
+	  (loop []
+        (when-let [line (.readLine ^java.io.BufferedReader reader)]
+		  (when-let [device (re-find #"Name\s+=\s(.*)" line)]
+            (swap! interfaces conj (second device))
+			)
+          (recur))))
+     (.waitFor process)
+	 @interfaces))
 
+(when (= :windows (jna-path/get-os))
+  (let [ready-chan (async/chan 1)]
+    (async/go-loop []
+      (let [interfaces (into ["(auto)"] (get-jackd-interfaces-windows))]
+        (println "[pae:jack:choose-interface]")
+		(println (apply str (map-indexed (fn [idx interface] (str " > " idx " " interface "\n")) interfaces)))
+		(println "Choose your audio-output device (type number from 0 to" (inc (count interfaces)) ")" )
+		(flush)
+          (let [choice (read-line)
+                choice (if (re-matches #"[1-9]+$" choice) 
+                         (Integer/parseInt choice) 
+                         false)]
+            (when-not (false? choice)
+              (if (zero? choice)
+                (spawn-jackd-windows! nil)
+                (spawn-jackd-windows! (nth interfaces choice)))))))
+    (async/<!! ready-chan)))
 
 (def jack-server ^Jack (Jack/getInstance))
 
